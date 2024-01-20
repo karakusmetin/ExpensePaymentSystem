0ac1c803c7d5fa6905227b9cff08226ee4c7406f
Metin Karakus <karakusmeetin@gmail.com>
2024-01-20 14:41:11 +0300

    ExpenseCategoryController added

---
+++ EPS.Api/Controlles/ExpenseCategoryController.cs
@@ -0,0 +1,68 @@
+﻿using EPS.Business.Cqrs;
+using EPS.Schema;
+using ESP.Base.Response;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EPS.Api.Controlles
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ExpenseCategoryController : ControllerBase
+	{
+		private readonly IMediator mediator;
+
+		public ExpenseCategoryController(IMediator mediator)
+		{
+			this.mediator = mediator;
+		}
+
+		[HttpGet]
+
+		public async Task<ApiResponse<List<ExpenseCategoryResponse>>> Get()
+		{
+			var operation = new GetAllExpenseCategoryQuery();
+			var result = await mediator.Send(operation);
+			return result;
+		}
+
+		[HttpGet("{id}")]
+
+		public async Task<ApiResponse<ExpenseCategoryResponse>> Get(int id)
+		{
+			var operation = new GetExpenseCategoryByIdQuery(id);
+			var result = await mediator.Send(operation);
+			return result;
+		}
+
+		[HttpPost]
+
+		public async Task<ApiResponse<ExpenseCategoryResponse>> Post([FromBody] ExpenseCategoryRequest Account)
+		{
+			var operation = new CreateExpenseCategoryCommand(Account);
+			var result = await mediator.Send(operation);
+			return result;
+		}
+
+		[HttpPut("{id}")]
+
+		public async Task<ApiResponse> Put(int id, [FromBody] ExpenseCategoryRequest Account)
+		{
+			var operation = new UpdateExpenseCategoryCommand(id, Account);
+			var result = await mediator.Send(operation);
+			return result;
+		}
+
+		[HttpDelete("{id}")]
+
+		public async Task<ApiResponse> Delete(int id)
+		{
+			var operation = new DeleteExpenseCategoryCommand(id);
+			var result = await mediator.Send(operation);
+			return result;
+		}
+	}
+}
